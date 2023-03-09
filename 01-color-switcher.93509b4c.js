31b09a0319ec16d0510bef827e03816cf4160d76
snevinchaniy <85674052+snevinchaniy@users.noreply.github.com>
2023-03-09 08:50:43 +0000

    Deploying to gh-pages from @ snevinchaniy/goit-js-hw-09@faea78755a13b38ada24daf34f71a2bbe2ef16a1 🚀

---
+++ 01-color-switcher.93509b4c.js
@@ -0,0 +1,2 @@
+!function(){var t={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]")};t.startBtn.addEventListener("click",(function(){t.startBtn.disabled=!0,t.stopBtn.disabled=!1,n=setInterval(a,1e3)})),t.stopBtn.addEventListener("click",(function(){t.startBtn.disabled=!1,t.stopBtn.disabled=!0,clearInterval(n)})),t.stopBtn.disabled=!0;var n=null;function a(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}}();
+//# sourceMappingURL=01-color-switcher.93509b4c.js.map
