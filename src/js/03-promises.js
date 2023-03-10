6c6608027fb273cc3a0bd99fb485b5bfe6653983
snevinchaniy <85674052+snevinchaniy@users.noreply.github.com>
2023-03-10 11:32:38 +0200

    added task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,52 @@
+import Notiflix from 'notiflix';
+
+// пошук елементів
+const refs = {
+  delay: document.querySelector('[name = delay]'),
+  step: document.querySelector('[name = step]'),
+  amount: document.querySelector('[name = amount]'),
+  createPromiseBtn: document.querySelector('button'),
+};
+
+// створення промісу
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+refs.createPromiseBtn.addEventListener('click', submitPromise);
+
+// відправка форми
+function submitPromise(e) {
+  e.preventDefault();
+
+  const delay = Number(refs.delay.value);
+  const step = Number(refs.step.value);
+  const amount = Number(refs.amount.value);
+
+  for (let i = 1; i <= amount; i += 1) {
+    const delayNew = delay + step * (i - 1);
+
+    createPromise(i, delayNew)
+      .then(({ position, delay }) => {
+        // console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        // console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
   }
 }
